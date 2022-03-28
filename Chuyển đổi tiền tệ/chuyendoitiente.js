b89e643c1522ebce6103cdf50699a05b96022560
Nguyen-Duy-Phat <ndfatcntt@gmail.com>
2022-03-28 17:02:04 +0700

    JS

---
+++ Chuyển đổi tiền tệ/chuyendoitiente.js
@@ -0,0 +1,8 @@
+function convert(){
+    let input = document.getElementById("input").value;
+    let sl = document.getElementById("sl").value;
+    let sl1 =document.getElementById("sl1").value;
+    let result;
+    result = input * sl1/sl;
+    document.getElementById("Result").innerText = "Result :" + result;
+}
---
+++ Hiển thị đầu ra/khaibaosudungbien.js
@@ -0,0 +1,12 @@
+// function do_something(){
+//     alert("Xin chào!")
+// }
+// function do_something(){
+//     document.write("Xin Chào!")
+// }
+// function do_something(){
+//     document.getElementById("showmaker").innerHTML = "Xin chào !"
+// }
+function gumaysusi(){
+    console.log("T19")
+}
