123848a9144cacafc23051bb53759baf0310bf46
Nguyen-Duy-Phat <ndfatcntt@gmail.com>
2022-03-29 10:00:14 +0700

    nhungjs

--- Nhúng JavaScrip/nhungjs.js
+++ Nhúng JavaScrip/nhungjs.js
@@ -1,8 +1,3 @@
-function doS() {
-    let input = document.getElementById('input').value;
-    let select1 = document.getElementById('select1').value;
-    let select2 = document.getElementById('select2').value;
-    // alert(document.getElementById('1'))
-    let result = input * select2/select1
-    document.getElementById('result').innerText = 'Result' + result;
+function showMessage(){
+    alert("Xin Chào");
 }
