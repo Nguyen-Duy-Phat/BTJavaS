592c86dd329373ad046e27c5d973f89eaa70ba90
Nguyen-Duy-Phat <ndfatcntt@gmail.com>
2022-03-29 13:42:17 +0700

    sdtt

---
+++ Sử dụng toán tử/sudungtoantu.js
@@ -0,0 +1,6 @@
+let widthhcn = prompt("Nhập vào chiều dài của hình chữ nhật :");
+let heighthcn = prompt("Nhập vào chiều cao hình chữ nhật");
+let width = parseInt(widthhcn);
+let height = parseInt(heighthcn);
+let dthcn = width * height;
+document.write("Diện tích hình chữ nhật : " + dthcn);
