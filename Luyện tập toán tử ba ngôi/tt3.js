e56061b9bbcc364972a7ae178a18805025e45b79
Nguyen-Duy-Phat <ndfatcntt@gmail.com>
2022-03-30 15:38:19 +0700

    tt3n

---
+++ Luyện tập toán tử ba ngôi/tt3.js
@@ -0,0 +1,7 @@
+// let a = prompt("a: ","");
+// let b = prompt("b: ","");
+// result = (a + b < 4) ? "Below" : "Over";
+// alert(result);
+let login = prompt("Nhập mã login : ","");
+let message = login=="Em" ? "Heloo Em" : login=="Di" ? "Heloo Di" : login=="" ? "No login" : "";
+alert(message);
