b967c81a78e2216bdcf715200198f788bc5a3e7d
Nguyen-Duy-Phat <ndfatcntt@gmail.com>
2022-04-14 07:50:04 +0700

    mt

--- Máy tính đơn giản/mtdg.js
+++ Máy tính đơn giản/mtdg.js
@@ -1,3 +1,12 @@
-function ikey(value){
-    document.getElementById("kq").innerHTML += value;
+
+function touch(value){
+    document.getElementById('input').value += value
+}
+function calculator() {
+    let input = document.getElementById('input').value;
+    let result = eval(input);
+    document.getElementById('input').value = result;
+}
+function clearTex() {
+    document.getElementById('input').value = '';
 }
